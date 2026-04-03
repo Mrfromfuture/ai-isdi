837be0021b8b93e8c45e2e540c6031fbff872369
Mrfromfuture <vuwork2853@gmail.com>
2026-04-03 19:54:34 +0500

    Assignment#2
    I have created loop in python

---
+++ ab.py
@@ -0,0 +1,19 @@
+# for i in range(100):
+#     print("abrar")
+#for i in range(3,100,2):
+#   print(i)
+#for i in range(3):
+#    p=input("username")
+ #   if p=="admin":
+  #      print("done")
+   #     break
+    #else:
+     #   print("wrong pasword")
+#C0UNT = 1
+#while COUNT<= 5:
+ #  print(COUNT)
+  #  COUNT += 2
+#for i in range(1000):
+ #   print("ali")
+for i in range (4,100,10):
+    print(i)
